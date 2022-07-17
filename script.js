ca32355773cf2870e7c5e4d79f911037ea126644
Alex Obadin <ialexobadin@gmail.com>
2022-07-17 20:56:01 +0300

    find solution for drawing

--- script.js
+++ script.js
@@ -3,21 +3,45 @@
 const squareField = document.getElementById('square-field');
 const square = document.createElement('div');
 square.classList.add('square-box');
-let squareNumber = 16;
 
-function getCssGridSquareProperty(squareNumber) {
+let squareNumber = 2500;
+
+function getCssGrid(squareNumber) {
   let square = Math.sqrt(squareNumber);
   return 'repeat' + '(' + square + ',' + '2fr' + ')';
 }
 
-console.log(getCssGridSquareProperty(squareNumber));
+function getChangeColor(event) {
+  let div = event.target.closest('div'); // (1)
 
-squareField.style.gridTemplateColumns = getCssGridSquareProperty(squareNumber);
+  if (!div) return; // (2)
+
+  if (!squareField.contains(div)) return; // (3)
+
+  div.classList.add('highlight');
+
+  // (4)
+}
 
 for (let i = 1; i <= squareNumber; i++) {
   squareField.appendChild(square.cloneNode(true));
-
-  console.log((i = +i));
 }
+squareField.style.gridTemplateColumns = getCssGrid(squareNumber);
 
-// squareField.style.gridTemplateColumns = 'repeat(2, 2fr)';
+console.log(getCssGrid(squareNumber));
+
+let hold = false;
+squareField.addEventListener('mousedown', function () {
+  hold = true;
+});
+
+squareField.addEventListener('mousemove', function (event) {
+  if (hold === true) {
+    getChangeColor(event);
+  }
+});
+
+window.addEventListener('mouseup', function () {
+  hold = false;
+  console.log(hold);
+});
