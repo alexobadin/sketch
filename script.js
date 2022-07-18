9a4cd508bd9419cd12acf8d48a720457c29b40f1
Alex Obadin <ialexobadin@gmail.com>
2022-07-18 12:00:49 +0300

    add grid choose

--- script.js
+++ script.js
@@ -3,9 +3,27 @@
 const squareField = document.getElementById('square-field');
 const square = document.createElement('div');
 square.classList.add('square-box');
+const gridSizeSelect = document.getElementById('grid-size');
 
-let squareNumber = 2500;
+let squareNumber;
 let hold = false;
+
+function gridTest() {
+  gridSizeSelect.addEventListener('change', function handleChange(event) {
+    squareNumber = event.target.value;
+
+    //Change grid template in CSS depending on set parameters and square root
+    squareField.style.gridTemplateColumns = getCssGrid(squareNumber);
+
+    //remove previous grid
+    squareField.innerHTML = '';
+
+    //Add multiply DIVs in root DIV
+    for (let i = 1; i <= squareNumber; i++) {
+      squareField.appendChild(square.cloneNode(true));
+    }
+  });
+}
 
 //Calculate the square root
 function getCssGrid(squareNumber) {
@@ -13,17 +31,12 @@ function getCssGrid(squareNumber) {
   return 'repeat' + '(' + square + ',' + '2fr' + ')';
 }
 
-//Change grid template in CSS depending on set parameters and square root
-squareField.style.gridTemplateColumns = getCssGrid(squareNumber);
-
-//Add multiply DIVs in root DIV
-for (let i = 1; i <= squareNumber; i++) {
-  squareField.appendChild(square.cloneNode(true));
-}
+gridTest();
 
 //Event delegation for use eventListener for change color every select DIVs
 function getChangeColor(event) {
   let div = event.target.closest('div');
+  console.log(div);
   if (!div) return;
   if (!squareField.contains(div)) return;
   div.classList.add('highlight');
@@ -33,13 +46,11 @@ function getChangeColor(event) {
 squareField.addEventListener('mousedown', function () {
   hold = true;
 });
-
 squareField.addEventListener('mousemove', function (event) {
   if (hold === true) {
     getChangeColor(event);
   }
 });
-
 window.addEventListener('mouseup', function () {
   hold = false;
   console.log(hold);
